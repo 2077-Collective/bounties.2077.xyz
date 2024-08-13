fc1791be0c02a666965bd842bda82125798c163b
Ian Guimaraes <ianguimaraes31@gmail.com>
2024-08-13 20:07:32 -0300

    chore: add skills

--- src/lib/server/schema.ts
+++ src/lib/server/schema.ts
@@ -4,13 +4,12 @@ import {
 	date,
 	integer,
 	pgTable,
-	primaryKey,
 	serial,
 	smallint,
 	text
 } from 'drizzle-orm/pg-core';
 
-export const sponsor = pgTable('sponsor', {
+export const sponsors = pgTable('sponsors', {
 	id: serial('id').primaryKey(),
 	displayName: text('name').notNull(),
 	username: text('name').notNull(),
@@ -23,8 +22,12 @@ export const sponsor = pgTable('sponsor', {
 	updatedAt: date('updated_at')
 });
 
-// TODO: missing skills
-export const user = pgTable('user', {
+export const skills = pgTable('skills', {
+	id: serial('id').primaryKey(),
+	name: text('name').notNull().unique()
+});
+
+export const users = pgTable('users', {
 	id: serial('id').primaryKey(),
 	displayName: text('name').notNull(),
 	username: text('name').notNull(),
@@ -37,11 +40,19 @@ export const user = pgTable('user', {
 	updatedAt: date('updated_at')
 });
 
-// TODO: missing skills needed
-export const bounty = pgTable('bounty', {
+export const userSkills = pgTable('user_skills', {
+	userId: integer('user_id')
+		.references(() => users.id)
+		.notNull(),
+	skillId: integer('skill_id')
+		.references(() => skills.id)
+		.notNull()
+});
+
+export const bounties = pgTable('bounties', {
 	id: serial('id').primaryKey(),
 	sponsorId: serial('sponsor_id')
-		.references(() => sponsor.id)
+		.references(() => sponsors.id)
 		.notNull(),
 	title: text('title').notNull(),
 	description: text('description').notNull(),
@@ -53,45 +64,48 @@ export const bounty = pgTable('bounty', {
 	updatedAt: date('updated_at')
 });
 
-// TODO: missing winner submission
-export const reward = pgTable('reward', {
+export const bountySkills = pgTable('bounty_skills', {
+	bountyId: integer('bounty_id')
+		.references(() => bounties.id)
+		.notNull(),
+	skillId: integer('skill_id')
+		.references(() => skills.id)
+		.notNull()
+});
+
+export const rewards = pgTable('rewards', {
 	id: serial('id').primaryKey(),
 	bountyId: integer('bounty_id')
-		.references(() => bounty.id)
+		.references(() => bounties.id)
 		.notNull(),
 	amount: bigint('amount', { mode: 'bigint' }).notNull(),
-	// winner: integer('winner').references(() => submission.),
+	winner: integer('winner').references(() => submissions.id),
 	createdAt: date('created_at').notNull(),
 	updatedAt: date('updated_at')
 });
 
-export const submission = pgTable(
-	'submission',
-	{
-		bountyId: integer('bounty_id')
-			.references(() => bounty.id)
-			.notNull(),
-		userId: integer('user_id')
-			.references(() => user.id)
-			.notNull(),
-		state: smallint('state').notNull().default(0),
-		link: text('link').notNull(),
-		details: text('description').notNull(),
-		createdAt: date('created_at').notNull(),
-		updatedAt: date('updated_at')
-	},
-	(table) => ({
-		pk: primaryKey({ columns: [table.bountyId, table.userId] })
-	})
-);
-
-export const comment = pgTable('comment', {
+export const submissions = pgTable('submissions', {
 	id: serial('id').primaryKey(),
 	bountyId: integer('bounty_id')
-		.references(() => bounty.id)
+		.references(() => bounties.id)
 		.notNull(),
 	userId: integer('user_id')
-		.references(() => user.id)
+		.references(() => users.id)
+		.notNull(),
+	state: smallint('state').notNull().default(0),
+	link: text('link').notNull(),
+	details: text('description').notNull(),
+	createdAt: date('created_at').notNull(),
+	updatedAt: date('updated_at')
+});
+
+export const comments = pgTable('comments', {
+	id: serial('id').primaryKey(),
+	bountyId: integer('bounty_id')
+		.references(() => bounties.id)
+		.notNull(),
+	userId: integer('user_id')
+		.references(() => users.id)
 		.notNull(),
 	content: text('content').notNull(),
 	createdAt: date('created_at').notNull(),
