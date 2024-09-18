4f24fa162b8b4bea7ccd0b28ccab874e19970bc2
Ian K. Guimaraes <ianguimaraes31@gmail.com>
2024-09-18 11:31:06 -0300

    refactor(ProfileImage): accept size as prop

--- src/lib/components/ProfileImage.svelte
+++ src/lib/components/ProfileImage.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
-	const { image }: { image?: string } = $props();
+	const { image, size = '36px' }: { image?: string; size?: string } = $props();
 </script>
 
 <img
 	src={image || '/images/placeholder-profile-image.webp'}
 	alt="User's profile"
-	class="w-9 h-9 rounded-full"
+	class="rounded-full"
+	style:width={size}
+	style:height={size}
 />
