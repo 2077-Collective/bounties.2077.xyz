3c927cb0ff8fba40083ebb70fbc680a8af918b0c
Ian K. Guimaraes <ianguimaraes31@gmail.com>
2024-09-18 11:32:07 -0300

    feat(sponsor): fetch public profile function

--- src/lib/server/database/sponsors.ts
+++ src/lib/server/database/sponsors.ts
@@ -1,8 +1,8 @@
 import { type SelectSponsor, UpdateSponsorSchema, type InsertSponsor } from '$lib/types';
-import { asc, eq } from 'drizzle-orm';
+import { asc, desc, eq, sql, sum } from 'drizzle-orm';
 import { db } from '.';
 import { z } from 'zod';
-import { sponsors } from '$lib/types/schema';
+import { bounties, rewards, sponsors, submissions, users } from '$lib/types/schema';
 
 export function createNewSponsor(sponsor: InsertSponsor) {
 	return db.insert(sponsors).values(sponsor).returning();
@@ -35,4 +35,62 @@ export async function updateSponsorById(id: number, sponsor: z.infer<typeof Upda
 	if (updatedSponsor.length === 0) return null;
 
 	return updatedSponsor[0];
+}
+
+export async function getSponsorPublicProfile(id: number) {
+	const sponsorPromise = db.query.sponsors.findFirst({
+		with: {
+			bounties: true
+		},
+		where: eq(sponsors.id, id)
+	});
+
+	const [sponsor, totalAmountAwarded, topContributor] = await Promise.all([
+		sponsorPromise,
+		getTotalAmountAwarded(id),
+		getTopContributor(id)
+	]);
+
+	if (!sponsor) return null;
+
+	return {
+		...sponsor,
+		totalAmountAwarded,
+		topContributor
+	};
+}
+
+async function getTotalAmountAwarded(id: number) {
+	const [{ totalAwarded }] = await db
+		.select({
+			totalAwarded: sum(rewards.amount)
+		})
+		.from(rewards)
+		.innerJoin(bounties, eq(bounties.id, rewards.bountyId))
+		.where(eq(bounties.sponsorId, id))
+		.limit(1)
+		.execute();
+
+	return totalAwarded;
+}
+
+async function getTopContributor(sponsorId: number) {
+	const result = await db
+		.select({
+			userId: submissions.userId,
+			displayName: users.displayName,
+			totalAmountAwarded: sql<number>`SUM(${rewards.amount})`
+		})
+		.from(rewards)
+		.innerJoin(bounties, eq(bounties.id, rewards.bountyId))
+		.innerJoin(submissions, eq(submissions.id, rewards.winner))
+		.innerJoin(users, eq(users.id, submissions.userId))
+		.where(eq(bounties.sponsorId, sponsorId))
+		.groupBy(submissions.userId, users.displayName)
+		.orderBy(desc(sql<number>`SUM(${rewards.amount})`))
+		.limit(1);
+
+	if (!result.length) return null;
+
+	return result[0];
 }
