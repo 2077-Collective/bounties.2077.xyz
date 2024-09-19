b181c61e5b3ef6133abe1a1b0750956fa7e1e115
Ian K. Guimaraes <ianguimaraes31@gmail.com>
2024-09-19 11:16:49 -0300

    feat(stores): populate account store on app layout

--- src/routes/app/+layout.svelte
+++ src/routes/app/+layout.svelte
@@ -1,10 +1,23 @@
 <script lang="ts">
 	import { Header } from '$lib/components/Header/index';
 	import ShortcutsHandler from '$lib/components/ShortcutHandler.svelte';
+	import { type Snippet } from 'svelte';
+	import { setAccount } from '$lib/stores/account.svelte';
+	import type { PageData } from './$types';
+
+	const {
+		data,
+		children
+	}: {
+		data: PageData;
+		children: Snippet;
+	} = $props();
+
+	setAccount(data.account);
 </script>
 
 <div class="w-full h-full">
 	<ShortcutsHandler />
 	<Header />
-	<slot />
+	{@render children()}
 </div>
