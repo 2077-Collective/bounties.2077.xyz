6be13304b0ce65a61be25f7c8b34a839bdf4d59c
Ian K. Guimaraes <ianguimaraes31@gmail.com>
2024-09-30 18:19:37 -0300

    feat(components): modal

---
+++ src/lib/components/Modal.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import { closeModal, getModal } from '$lib/stores/modal.svelte';
+	import { X } from 'lucide-svelte';
+	const modal = getModal();
+</script>
+
+{#if modal.open && modal.component}
+	<div class="absolute inset-0 bg-black/30 backdrop-blur-sm z-10 flex items-center justify-center">
+		<div class="bg-white relative z-20 w-5/6 lg:auto lg:max-w-2xl rounded-lg p-8">
+			{#if modal.options?.dismissible}
+				<button onclick={closeModal} class="absolute right-8 p-2 rounded-full border top-6">
+					<X class="w-3 h-3" />
+				</button>
+			{/if}
+
+			{@render modal.component()}
+		</div>
+	</div>
+{/if}
