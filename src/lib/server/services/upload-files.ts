9c4f11ab70b677dabe0346709b11888d508fd8e3
Ian K. Guimaraes <ianguimaraes31@gmail.com>
2024-10-04 13:53:54 -0300

    feat: markdown support for bounty description

--- src/lib/server/services/upload-files.ts
+++ src/lib/server/services/upload-files.ts
@@ -2,12 +2,26 @@ import { BLOB_READ_WRITE_TOKEN } from '$env/static/private';
 import { put } from '@vercel/blob';
 import { nanoid } from 'nanoid';
 
+// TODO: should throw if not image
 export async function uploadImage(image: File): Promise<string> {
 	if (image.size > 4 * 1024 * 1024) {
 		throw new Error('Image too large');
 	}
 
 	const { url } = await put(nanoid(10), image, {
+		access: 'public',
+		token: BLOB_READ_WRITE_TOKEN
+	});
+
+	return url;
+}
+
+export async function uploadAnyFile(file: File): Promise<string> {
+	if (file.size > 4 * 1024 * 1024) {
+		throw new Error('Image too large');
+	}
+
+	const { url } = await put(nanoid(10), file, {
 		access: 'public',
 		token: BLOB_READ_WRITE_TOKEN
 	});
